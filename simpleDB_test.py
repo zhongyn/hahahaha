d385e0cd4f224ccadac66d404091057916235cfc
Yaonan Zhong <zhongyaonan@gmail.com>
2016-01-10 18:53:22 -0600

    add patchers

--- simpleDB_test.py
+++ simpleDB_test.py
@@ -17,26 +17,29 @@ class SimpleDBTestCase(unittest.TestCase):
 		self.numequalto_10 = ['NUMEQUALTO', '10']
 		self.end = ['END']
 
-	@patch('simpleDB.SimpleDB.get_command')
-	@patch('sys.stdout', new_callable=StringIO)
-	def test_set_var_get_var(self, mock_output, mock_command):
-		mock_command.side_effect = [self.set_a_10, self.get_a, self.end]
+		self.patcher_get_cmd = patch('simpleDB.SimpleDB.get_command')
+		self.patcher_output = patch('sys.stdout', new_callable=StringIO)
+		self.mock_command = self.patcher_get_cmd.start()
+		self.mock_output = self.patcher_output.start()
+	
+	def tearDown(self):
+		self.patcher_get_cmd.stop()
+		self.patcher_output.stop()
+	
+	def test_set_var_get_var(self):
+		self.mock_command.side_effect = [self.set_a_10, self.get_a, self.end]
 		self.db.run()
-		self.assertEqual('10\n', mock_output.getvalue())
+		self.assertEqual('10\n', self.mock_output.getvalue())
 
-	@patch('simpleDB.SimpleDB.get_command')
-	@patch('sys.stdout', new_callable=StringIO)
-	def test_get_var_no_exist(self, mock_output, mock_command):
-		mock_command.side_effect = [self.get_a, self.end]
+	def test_get_var_no_exist(self):
+		self.mock_command.side_effect = [self.get_a, self.end]
 		self.db.run()
-		self.assertEqual('NULL\n', mock_output.getvalue())
+		self.assertEqual('NULL\n', self.mock_output.getvalue())
 
-	@patch('simpleDB.SimpleDB.get_command')
-	@patch('sys.stdout', new_callable=StringIO)
-	def test_set_var_unset_var_get_var(self, mock_output, mock_command):
-		mock_command.side_effect = [self.set_a_10, self.unset_a, self.get_a, self.end]
+	def test_set_var_unset_var_get_var(self):
+		self.mock_command.side_effect = [self.set_a_10, self.unset_a, self.get_a, self.end]
 		self.db.run()
-		self.assertEqual('NULL\n', mock_output.getvalue())
+		self.assertEqual('NULL\n', self.mock_output.getvalue())
 
 
 if __name__ == '__main__':
